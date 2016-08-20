6b23aaccd8e6b69ad9090ff217fed87cfeaa4f07
Jen J Wong <jenjwong@gmail.com>
2016-08-19 21:39:25 -0700

    Implement isPallindrom and passes all tests

--- run.js
+++ run.js
@@ -1,1 +1,55 @@
-exports.isPallindrom = string => true;
+
+
+(function() {
+  'use strict';
+  // const _ = require('lodash');
+
+  exports.isPallindrom = string => {
+    let stringIsPallindrom = true;
+
+    for (let i = 0; i < Math.floor(string.length/2); i++) {
+    let a = string[i];
+    let b = string[string.length - i -1]
+       if (a !== b) {
+        stringIsPallindrom = false;
+      } 
+    }
+    return stringIsPallindrom;
+  };
+
+
+
+
+
+}());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // let stringIsPallindrom = true;
+  // _.forEach(string, (letter, index, collection) => {
+
+
+  //   console.log(Math.floor(string.length/2))
+  //   if (string[index] !== string[string.length - index + 1]) {
+  //     stringIsPallindrom = false;
+  //   } 
+  //   if (string.length % 2 !==0 && index === Math.floor(string.length/2)) {
+  //     stringIsPallindrom = true;
+  //   }
+  // });
+  // return stringIsPallindrom;
--- test/test.js
+++ test/test.js
@@ -1,17 +1,24 @@
-"use strict";
-
-const _ = require('lodash');
-const expect = require('chai').expect;
-const algo = require('../run.js');
 
 
+(function() {
+  'use strict';
 
-describe('isPallindrom', () => {
-  let input = { 'abba': true,
-                'aba': true };
-    _.forEach(input, function(value, key) {
-      it(`it ${key} should return ${value}`, () => {
-        expect(algo.isPallindrom(key)).to.equal(value);
-      })
-    })
-});
+  const _ = require('lodash');
+  const expect = require('chai').expect;
+  const algo = require('../run.js');
+
+  describe('isPallindrom', () => {
+    let input = { 'abba': true,
+                  'aba': true,
+                  'a': true,
+                  'aws': false,
+                  'awdfdss': false,
+                  'xxx': true};
+    _.forEach(input, (value, key) => {
+      it(`input ${key} should return ${value}`, () => {
+        let testFunc = algo.isPallindrom(key);
+        expect(testFunc).to.equal(value);
+      });
+    });
+  });
+}());

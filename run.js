7bd3f73ee38faaa22f41282479c3129ab5b4914b
Jen Wong <jenjwong@gmail.com>
2016-09-18 00:25:07 -0700

    Update run.js

--- run.js
+++ run.js
@@ -1,7 +1,4 @@
-
-
-
-  'use strict';
+ 'use strict';
   const _ = require('lodash');
 
   exports.isPallindrom = string => {
@@ -46,26 +43,3 @@
     }
     return true;
   };
-
-
-
-
-
-
-
-
-
-
-  // let stringIsPallindrom = true;
-  // _.forEach(string, (letter, index, collection) => {
-
-
-  //   console.log(Math.floor(string.length/2))
-  //   if (string[index] !== string[string.length - index + 1]) {
-  //     stringIsPallindrom = false;
-  //   } 
-  //   if (string.length % 2 !==0 && index === Math.floor(string.length/2)) {
-  //     stringIsPallindrom = true;
-  //   }
-  // });
-  // return stringIsPallindrom;
